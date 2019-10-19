c42eab9daeee4724759d5b3258730f11c6c5cd00
minakshi-chidrawar <minakshichidrawar@gmail.com>
2019-10-19 16:26:11 +0100

    Migration for the shortUrl

---
+++ database/migrations/2019_10_19_141632_create_short_urls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url', 255);
+            $table->string('shortCode', 255);
+            $table->bigInteger('hits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_urls');
+    }
+}
