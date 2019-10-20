43ab2f4c74e55ce85d56d9428e3b5b7df6116ef5
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-10-20 19:48:43 +0100

    Storage path

--- storage/framework/views/e13093f64f2d9af5560082305dfa8dec04729219.php
+++ storage/framework/views/e13093f64f2d9af5560082305dfa8dec04729219.php
@@ -12,23 +12,25 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
     </head>
     <body>
         <div style="margin-top: 15%;">        
         <div class="container d-flex h-100">
             <div class="row align-self-center w-100">
-                <div class="col-6 mx-auto">
+                <div class="col-12 mx-auto">
                     <div class="jumbotron">
-                        <h3 class="display-4">Check URL</h1>
+                        <h4>Provide URL</h4>
                         
                         <form action="home" method="POST" class="pb-5">
                         <?php echo csrf_field(); ?>
-                            <div class="input-group">
-                                <input type="url" name="url" id="url" required>
+                            <div class="row">
+                                <div class="col-8">
+                                    <input type="url" name="url" id="url" placeholder="Shorten your link" class="form-control" autocomplete="off" required>
+                                </div>
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-primary">Shorten</button>
+                                </div>
                             </div>
-
-                            <button type="submit">Check</button>
                         </form>
 
                         <div>
