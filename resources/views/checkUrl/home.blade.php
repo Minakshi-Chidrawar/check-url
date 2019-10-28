1920c08cdc7eb26b4d351e16ced981d3f4ed090f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-10-28 20:01:17 +0000

    Removed pb-5 class

--- resources/views/checkUrl/home.blade.php
+++ resources/views/checkUrl/home.blade.php
@@ -16,46 +16,48 @@
     </head>
     <body>
         <div style="margin-top: 15%;">        
-        <div class="container d-flex justify-content-center h-100">
-            <div class="row align-items-center w-100">
-                <div class="col-12 mx-auto">
-                    <div class="jumbotron bg-blue">
-                        @if(!empty($message))
-                            <div class="row">
-                                <div class="col-9">
-                                    <div class="input-group input-group-lg">
-                                        <input class="form-control" id="disabledInput" type="text" value="{{ $message }}">
-                                    </div>
-                                </div>
-                                <div class="col-3">
-                                    <button onclick="copyFunction()" class="btn btn-primary btn-lg">Copy</button>
-                                </div>
-                            </div>
-                        @else
-                            <form action="/" method="POST" class="pb-5" id="InputUrl" autocomplete="off">
-                            @csrf
-                                <div class="row" id ="divTest">
-                                    <div class="col-9">
-                                        <div class="input-group input-group-lg">
-                                            <input type="url" name="url" id="url" value="{{ Request::old('url') }}" placeholder="Shorten your link" class="form-control" required>
+            <div class="container">
+                <div class=" d-flex justify-content-center h-100">
+                    <div class="row align-items-center w-100">
+                        <div class="col-12 mx-auto">
+                            <div class="jumbotron bg-blue">
+                                @if(!empty($message))
+                                    <div class="row">
+                                        <div class="col-9">
+                                            <div class="input-group input-group-lg">
+                                                <input class="form-control" id="disabledInput" type="text" value="{{ $message }}">
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <button onclick="copyFunction()" class="btn btn-primary btn-lg">Copy</button>
                                         </div>
                                     </div>
-                                    <div class="col-3">
-                                        <button type="submit" id="shorten" class="btn btn-primary btn-lg">Shorten</button>
-                                    </div>
-                                </div>
-                            </form>
-                            @if ($message = Session::get('error'))
-                                <div class="alert alert-danger alert-block">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>	
-                                        <strong>{!! $message !!}</strong>
-                                </div>
-                            @endif
-                        @endif
+                                @else
+                                    <form action="/" method="POST" id="InputUrl" autocomplete="off">
+                                        @csrf
+                                        <div class="row" id ="divTest">
+                                            <div class="col-9">
+                                                <div class="input-group input-group-lg">
+                                                    <input type="url" name="url" id="url" value="{{ Request::old('url') }}" placeholder="Shorten your link" class="form-control" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-3">
+                                                <button type="submit" id="shorten" class="btn btn-primary btn-lg">Shorten</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    @if ($message = Session::get('error'))
+                                        <div class="alert alert-danger alert-block">
+                                            <button type="button" class="close" data-dismiss="alert">×</button>	
+                                                <strong>{!! $message !!}</strong>
+                                        </div>
+                                    @endif
+                                @endif
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
         <script>
             function copyFunction()
